9c64805f9c63cd7d6c0c7e20376ca2bc9c3fe183
Iván Pérez <ivanpg28@gmail.com>
2019-04-29 19:11:54 +0200

    Añadidas variables, constructores y getters y setters a la clase NodoLista

--- src/proyectoed/NodoLista.java
+++ src/proyectoed/NodoLista.java
@@ -10,5 +10,47 @@ package proyectoed;
  * @author iperez10954
  */
 public class NodoLista {
+    private String clave;
+    private Arbol raiz;
+    private NodoLista nodo;
+
+    public NodoLista() {
+        clave = null;
+        raiz = null;
+        nodo = null;
+    }
+
+    public NodoLista(String clave, Arbol raiz, NodoLista nodo) {
+        this.clave = clave;
+        this.raiz = raiz;
+        this.nodo = nodo;
+    }
+    
+    
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Arbol getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Arbol raiz) {
+        this.raiz = raiz;
+    }
+
+    public NodoLista getNodo() {
+        return nodo;
+    }
+
+    public void setNodo(NodoLista nodo) {
+        this.nodo = nodo;
+    }
+    
     
 }
